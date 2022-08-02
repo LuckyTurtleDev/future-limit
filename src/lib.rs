255191a7b630592cfca46bb1a8209c4109d2fcc5
LuckyTurtleDev <git@lukas1818.de>
2022-08-02 19:23:15 +0200

    chaneg api

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,60 @@
+use async_trait::async_trait;
 use std::{future::Future, sync::Arc};
 use tokio::{sync::Mutex, task::yield_now};
 
-pub struct Limiter<F>
-where
-	F: Future,
-{
-	future: F,
-	max_parallelism: usize,
-	current_parallelism: Arc<Mutex<usize>>,
+struct LimiterState {
+	current_parallelism: usize,
 }
 
-impl<F> Limiter<F>
+pub struct Limiter {
+	max_parallelism: usize,
+	state: Arc<Mutex<LimiterState>>,
+}
+
+#[async_trait]
+trait ToName<F>
+where
+	Self: Future,
+{
+	async fn limit(self, limits: &Vec<Limiter>) -> <Self as Future>::Output;
+}
+
+#[async_trait]
+impl<F> ToName<F> for F
 where
 	F: Future,
+	F: Send,
+	<Self as Future>::Output: Send,
 {
-	pub async fn limit(self) -> <F as Future>::Output {
-		loop {
-			let mut current_parallelism = self.current_parallelism.lock().await;
-			if self.max_parallelism > *current_parallelism {
-				*current_parallelism += 1;
-				break;
+	async fn limit(self, limits: &Vec<Limiter>) -> <Self as Future>::Output {
+		let states = loop {
+			//look all mutex at the same time
+			let mut mutexs = Vec::new();
+			for limit in limits {
+				match limit.state.try_lock() {
+					Err(_) => break,
+					Ok(state) => {
+						if limit.max_parallelism <= state.current_parallelism {
+							break;
+						}
+						mutexs.push(state)
+					},
+				}
 			}
-			yield_now().await
+			if mutexs.len() == limits.len() {
+				break mutexs;
+			}
+			yield_now().await;
+		};
+		for mut state in states {
+			state.current_parallelism += 1;
+			drop(state);
 		}
-		self.future.await
+		let return_value = self.await;
+		for limit in limits {
+			let mut state = limit.state.lock().await;
+			state.current_parallelism = state.current_parallelism - 1;
+		}
+		return_value
 	}
 }
